c077dff3d423b836099fce892b5604a43d62cf9e
Ahmed Sabry <63425800+DexterTheDev@users.noreply.github.com>
2025-11-10 14:16:54 +0200

    Delete Refeurzo directory

--- Refeurzo/models/emails.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require('mongoose');
-
-module.exports = mongoose.model('emails', new mongoose.Schema({
-    email: { type: String, required: true }
-}));
--- Refeurzo/routes/route[get]/dynamic/tos.js
+++
@@ -1,9 +0,0 @@
-const index = async (fastify, options, done) => {
-  fastify.get("/tos", async (req, res) => {
-    return req.render("/dynamic/tos.liquid");
-  });
-
-  done();
-};
-
-module.exports = index;
